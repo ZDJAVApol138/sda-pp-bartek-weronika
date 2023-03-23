84a337520cc31ca234d9e6ecaab2c71f1c76029e
szjozi <szjozefczuk.sda@gmail.com>
2023-03-23 19:01:19 +0100

    README.md added

--- src/main/java/pl/sda/bankapp/Main.java
+++ src/main/java/pl/sda/bankapp/Main.java
@@ -1,8 +1,15 @@
 package pl.sda.bankapp;
 
+import pl.sda.bankapp.model.Address;
+
 public class Main {
 
     public static void main(String[] args) {
+//hashcode oraz equals
+        Address address1 = new Address("Amsterdam","Amsterdamska","00-001");
+        Address address2 = new Address("Barcelona","Barcelońska","00-002");
+        System.out.println(address1.toString());
+
 
     }
 }
