9f9b8a3633b34cbb241f9e1802b459e53b33a7bf
bartekdziedzic <bartekkdziedzic@gmail.com>
2023-04-18 21:14:01 +0200

    task 14: BankService Logic

--- src/main/java/pl/sda/bankapp/Main.java
+++ src/main/java/pl/sda/bankapp/Main.java
@@ -15,9 +15,9 @@ public class Main {
         // System.out.println(address1.toString());
 
 
-        Customer customer1 = new Customer("Alicja", "Artemska", "1111111111", "aa@aa.aa", 1, 11, address1, "111111");
-        StandardAccount acc1 = new StandardAccount(123, 1234,  Currency.EUR);
-        PremiumAccount acc2 = new PremiumAccount(456, 4321,  Currency.EUR);
+        Customer customer1 = new Customer();
+        StandardAccount acc1 = new StandardAccount(Currency.EUR);
+        PremiumAccount acc2 = new PremiumAccount( Currency.EUR);
         customer1.addAccount(acc1);
         customer1.addAccount(acc2);
         customer1.listAccount();
--- src/main/java/pl/sda/bankapp/service/BankService.java
+++ src/main/java/pl/sda/bankapp/service/BankService.java
@@ -1,7 +1,12 @@
 package pl.sda.bankapp.service;
 
+import pl.sda.bankapp.model.Address;
 import pl.sda.bankapp.model.Bank;
+import pl.sda.bankapp.model.Customer;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BankService {
@@ -14,5 +19,52 @@ public class BankService {
         this.scanner = new Scanner(System.in);
         this.bank = bank;
         this.accountFactory = new AccountFactory();
+    }
+
+    public void listCustomers(ArrayList<Customer> customers) {
+        if (bank.getCustomers().size() == 0) {
+            System.out.println("Customers list is empty!");
+            return;
+        }
+        System.out.println("Customer list");
+        bank.listCustomer();
+    }
+
+    private String getString(String text) {
+        System.out.println(text);
+        return scanner.nextLine();
+    }
+
+    public void createCustomer() {
+
+        String name = getString("Name:");
+        String surname = getString("Surame:");
+        String phone = getString("Phone:");
+        String email = getString("Email:");
+        String pesel = getString("Pesel:");
+        String city = getString("City:");
+        String street = getString("Street:");
+        String postCode = getString("Postal Code:");
+
+        String dateOfBirthSt = getString("Date of birth (dd/mm/yyyy:");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateOfBirth = LocalDate.parse(dateOfBirthSt, formatter);
+
+        Address address = new Address(city, street, postCode);
+
+        Customer customer = new Customer(name,surname,phone,email,pesel,address,dateOfBirth);
+        bank.addCustomer(customer);
+    }
+
+    public void removeCustomerByPesel() {
+        String pesel = scanner.nextLine();
+        Customer customer = bank.findCustomer(pesel);
+        bank.getCustomers().remove(customer);
+    }
+
+    public void getCustomerByPesel() {
+        String pesel = scanner.nextLine();
+        Customer customer = bank.findCustomer(pesel);
+        bank.getCustomers().add(customer);
     }
 }
